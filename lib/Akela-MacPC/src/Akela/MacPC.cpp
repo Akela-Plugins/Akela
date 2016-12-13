5698c6b478a28559e2d625790c2d8e12c941376c
Gergely Nagy <algernon@madhouse-project.org>
2016-12-13 09:05:19 +0100

    MacPC: New plugin
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ lib/Akela-MacPC/src/Akela/MacPC.cpp
@@ -0,0 +1,46 @@
+/* -*- mode: c++ -*-
+ * Akela -- Animated Keyboardio Extension Library for Anything
+ * Copyright (C) 2016  Gergely Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "MacPC.h"
+
+namespace Akela {
+
+  MacPC::MacPC (void) {
+    event_handler_hook_add (this->eventHandlerHook);
+  }
+
+  void
+  MacPC::setup (void) {
+    ::HostOS.setup ();
+  }
+
+  bool
+  MacPC::eventHandlerHook (Key mappedKey, byte row, byte col, uint8_t keyState) {
+    if (keyState & INJECTED)
+      return false;
+
+    if (mappedKey.raw != Key_AltCmd.raw)
+      return false;
+
+    if (::HostOS.os () != HostOS::MACOS)
+      return false;
+
+    handle_key_event (Key_LGUI, row, col, keyState | INJECTED);
+    return true;
+  }
+};
