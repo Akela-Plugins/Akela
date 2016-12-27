83e6905ff24429fdce51dff06d6ab662be2b4979
Gergely Nagy <algernon@madhouse-project.org>
2016-12-27 13:21:37 +0100

    tools/flash.sh: Make avrdude less verbose
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- tools/flash.sh
+++ tools/flash.sh
@@ -9,4 +9,4 @@ read a
 
 stty -F /dev/ttyACM* 1200 hupcl
 sleep 3s
-avrdude -v -patmega32u4 -cavr109 -D -P /dev/ttyACM* -b57600 -Uflash:w:firmware/$1/$1-latest.hex:i
+avrdude -q -q -patmega32u4 -cavr109 -D -P /dev/ttyACM* -b57600 -Uflash:w:firmware/$1/$1-latest.hex:i
