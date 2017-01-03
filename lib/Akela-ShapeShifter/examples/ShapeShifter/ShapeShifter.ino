b1830aea3f5d0e277cfacd645ae1ae745f68cb59
Gergely Nagy <algernon@madhouse-project.org>
2017-01-03 09:59:12 +0100

    Clean up the example sketches
    Fixes #124.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- lib/Akela-ShapeShifter/examples/ShapeShifter/ShapeShifter.ino
+++ lib/Akela-ShapeShifter/examples/ShapeShifter/ShapeShifter.ino
@@ -28,15 +28,15 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_PageDn,        Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Esc,
 
    Key_LCtrl, Key_Backspace, Key_LGUI, Key_LShift,
-   Key_KeymapNext_Momentary,
+   Key_skip,
 
-   Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Keymap2,
+   Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
    Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
               Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
    Key_RShift, Key_RAlt, Key_Space, Key_RCtrl,
-   Key_KeymapNext_Momentary
+   Key_skip
    ),
 };
 
--- lib/Akela-TapDance/examples/TapDance/TapDance.ino
+++ lib/Akela-TapDance/examples/TapDance/TapDance.ino
@@ -23,20 +23,20 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   [0] = KEYMAP_STACKED
   (
    Key_LEDEffectNext, Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick,      Key_Q, Key_W, Key_E, Key_R, Key_T, TD(0),
+   Key_Backtick,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,        Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDn,        Key_Z, Key_X, Key_C, Key_V, Key_B, TD(1),
+   Key_PageDn,        Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Esc,
 
    Key_LCtrl, Key_Backspace, Key_LGUI, Key_LShift,
-   Key_KeymapNext_Momentary,
+   TD(0),
 
-   Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Keymap2,
+   Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
    Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
               Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
    Key_RShift, Key_RAlt, Key_Space, Key_RCtrl,
-   Key_KeymapNext_Momentary
+   TD(1)
    ),
 };
 
